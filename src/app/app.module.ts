4bd63f18498b57fd0edea28f29a9724016ff51e1
stevekibe <stevekibe2345@gmail.com>
2023-12-23 14:20:48 +0300

    not found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FormsModule } from '@angular/forms';
 import { TagsComponent } from './tags/tags.component';
 import { FoodPageComponent } from './food-page/food-page.component';
 import { CartPageComponent } from './cart-page/cart-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { CartPageComponent } from './cart-page/cart-page.component';
     TagsComponent,
     FoodPageComponent,
     CartPageComponent,
+    NotFoundComponent,
 
   ],
   imports: [
